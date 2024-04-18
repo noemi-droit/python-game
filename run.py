7c99cf58ec6babe04055ff11106e2f2ec832c69c
noemi-droit llampa <noemillampa50@gmail.com>
2024-04-18 15:30:33 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-",]
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+# printing the game board
+
+# take player inputxx
+# check for win or tie
+# switch the player
+# check  for win or tie again
