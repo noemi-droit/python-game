80778466acfb7aacbb5c2f26142d5b790970236f
noemi-droit llampa <noemillampa50@gmail.com>
2024-04-18 15:52:20 +0000

    Add the board to python structure

--- run.py
+++ run.py
@@ -4,8 +4,18 @@ board = ["-", "-", "-",
 currentPlayer = "X"
 winner = None
 gameRunning = True
-
 # printing the game board
+def printBoard(board):
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print("--------")
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print("--------")
+    print(board[6] + "|" + board[7] + "|" + board[8])
+
+
+
+
+
 
 # take player inputxx
 # check for win or tie
